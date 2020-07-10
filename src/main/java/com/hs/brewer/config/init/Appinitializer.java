b5a00ea50649e3dc05e3e2c69e03fdc49790aee2
AlissonSampaio <alisson2012alves@gmail.com>
2020-07-10 11:39:17 -0300

    Passando pro git hub

---
+++ src/main/java/com/hs/brewer/config/init/Appinitializer.java
@@ -0,0 +1,38 @@
+package com.hs.brewer.config.init;
+
+import javax.servlet.Filter;
+
+import org.springframework.web.filter.CharacterEncodingFilter;
+import org.springframework.web.servlet.support.AbstractAnnotationConfigDispatcherServletInitializer;
+
+import com.hs.brewer.config.WebConfig;
+
+public class Appinitializer extends AbstractAnnotationConfigDispatcherServletInitializer {
+
+	@Override
+	protected Class<?>[] getRootConfigClasses() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	protected Class<?>[] getServletConfigClasses() {
+		// TODO Auto-generated method stub
+		return new Class<?>[] {WebConfig.class};
+	}
+
+	@Override
+	protected String[] getServletMappings() {
+		// TODO Auto-generated method stub
+		return new String[] {"/"};
+	}
+	
+	@Override
+	protected Filter[] getServletFilters() {
+		CharacterEncodingFilter characterEncodingFilter = new CharacterEncodingFilter();
+		characterEncodingFilter.setEncoding("UTF-8");
+		characterEncodingFilter.setForceEncoding(true);
+		
+		return new Filter[] {characterEncodingFilter};
+		}
+}
